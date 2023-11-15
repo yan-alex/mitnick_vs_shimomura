48b12d31af248dba412ec6159f523f14ee9b1591
Yan Lanna Alexandre <yanlanna2@hotmail.com>
2023-11-15 16:45:13 +0100

    Initial Commit

---
+++ solutionXcode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  solutionXcode
+//
+//  Created by Yan Lanna Alexandre on 15/11/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
